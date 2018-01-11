9d50b20f94e57010f5b0633ab8052f657f277042
Tony Yoon <tooonnyy@gmail.com>
2018-01-11 15:26:18 -0500

    all organisms component loads correctly

--- client/components/Organisms.js
+++ client/components/Organisms.js
@@ -11,9 +11,9 @@ export default class Organisms extends Component {
     }
 
     componentDidMount(){
-        // axios.get('/api/organisms')
-        //     .then(res => res.data)
-        //     .then(organisms => this.setState({organisms}))
+        axios.get('/api/organisms')
+            .then(res => res.data)
+            .then(organisms => this.setState({organisms}))
     }
     
     render(){
@@ -21,24 +21,39 @@ export default class Organisms extends Component {
             <div>
                 <h1>ALL ORGANISMS COMPONENT</h1>
                 <div>
-                    {/* {
+                    <button><Link to={`/addform`}>Add Organism</Link></button>
+                </div>
+                <div>
+                    {
                         this.state.organisms.map(organism=>{
 
                             return (
-                                <div key={organism.id}>
-                                    <Link to={`/organisms/${organism.id}`} >
-                                        <div>
-                                            {organism.species}
-                                        </div>
-                                    </Link>
+                              <Link to={`/organisms/${organism.id}`} >
+                                <div key={organism.id} className="organism-container">
+                                    
+
+                                          <div className="organism-left">
+                                            <img src={organism.image} alt=""/>
+                                          </div>
+                                          <div className="organism-right">
+                                            <h1>Species - {organism.species}</h1>
+                                            <ul>
+                                              <li><strong>Kingdom: </strong>{organism.kingdom}</li>
+                                              <li><strong>Phylum: </strong>{organism.phylum}</li>
+                                              <li><strong>Classes: </strong>{organism.classes}</li>
+                                              <li><strong>Order: </strong>{organism.order}</li>
+                                              <li><strong>Family: </strong>{organism.family}</li>
+                                              <li><strong>Genus: </strong>{organism.genus}</li>
+                                            </ul>
+                                          </div>
+                                      
                                 </div>
+                              </Link>
                             )
                         })
-                    } */}
+                    }
                 </div>
-                <div>
-                    <button><Link to={`/addform`}>Add Organism</Link></button>
-                </div>
+                
             </div>
         )
     }
